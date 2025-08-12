b75d9396fd4d4f134ca69d7eadc97b6263d69b2d
wojtekbujak <wojtekbujak@o2.pl>
2025-08-12 21:41:55 +0200

    #

--- Dog_hotel/settings.py
+++ Dog_hotel/settings.py
@@ -124,8 +124,6 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
